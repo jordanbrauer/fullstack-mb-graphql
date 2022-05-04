34550cc291476996046598e6bb70681d666d7177
jordanbrauer <jordan.brauer@neofinancial.com>
2022-05-04 16:42:57 -0500

    slides and data loader stuff

--- app/planets/dataloader.go
+++ app/planets/dataloader.go
@@ -16,19 +16,19 @@ func Load(ctx context.Context, id int) (*Schema, error) {
 	return result.(*Schema), err
 }
 
-var loader = dataloader.NewBatchedLoader(func(ctx context.Context, k dataloader.Keys) []*dataloader.Result {
+var loader = dataloader.NewBatchedLoader(func(ctx context.Context, loading dataloader.Keys) []*dataloader.Result {
+	results := make([]*dataloader.Result, 0)
 	query, err := app.Queries.ReadFile("database/planet_batch.sql")
 
 	if err != nil {
 		log.Panic("cannot read query for data loader", err)
+
+		return results
 	}
 
-	results := make([]*dataloader.Result, 0)
-	ids := strings.Join(k.Keys(), ",")
-	body := strings.ReplaceAll(string(query), "?", ids)
-	rows, err := app.Database().Query(body)
-
-	log.Printf("Querying planets for %d character(s)\n", len(k.Keys()))
+	rows, err := app.Database().Query(
+		strings.ReplaceAll(string(query), "?", strings.Join(loading.Keys(), ",")),
+	)
 
 	if err != nil {
 		log.Panic(err)
@@ -36,23 +36,26 @@ var loader = dataloader.NewBatchedLoader(func(ctx context.Context, k dataloader.
 		return results
 	}
 
-	m := map[string]*Schema{}
+	planets := map[string]*Schema{}
+	errors := map[string]error{}
 
 	defer rows.Close()
 
 	for rows.Next() {
-		result := new(Schema)
-		err = rows.Scan(&result.ID, &result.Name)
-
-		if err != nil {
-			log.Panic(err.Error())
-		}
-
-		m[fmt.Sprintf("%d", result.ID)] = result
+		planet := new(Schema)
+		err = rows.Scan(&planet.ID, &planet.Name)
+		key := fmt.Sprintf("%d", planet.ID)
+		errors[key] = err
+		planets[key] = planet
 	}
 
-	for _, key := range k.Keys() {
-		results = append(results, &dataloader.Result{Data: m[key]})
+	log.Printf("Queried %d planet(s) for %d character(s)\n", len(planets), len(loading.Keys()))
+
+	for _, key := range loading.Keys() {
+		results = append(results, &dataloader.Result{
+			Data:  planets[key],
+			Error: errors[key],
+		})
 	}
 
 	return results
